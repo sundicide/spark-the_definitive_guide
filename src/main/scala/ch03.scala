6f322f0ee780d0f0d99885cc39f40971373bb353
sundicide <kyusuen@gmail.com>
2024-08-09 18:33:37 +0900

    study: TIL

--- src/main/scala/ch03.scala
+++ src/main/scala/ch03.scala
@@ -16,7 +16,7 @@ object ch03 {
      * Get the path to the resources folder
      * 참고로 getClass 함수는 spark.implicits._를 수행하기 전에 수행해야 한다.
      */
-    val resourceDataPath = getClass.getResource("/data/").getPath
+    val resourceDataPath = getClass.getResource("/data").getPath
 
     val flightFilePath = (s"$resourceDataPath/flight-data/parquet/2010-summary.parquet/")
     val flightsDF = spark.read.parquet(flightFilePath)
@@ -35,16 +35,96 @@ object ch03 {
     /**
      * Structured Streaming
      */
+    def structuredStreaming() = {
+      val staticDataFrame = spark.read.format("csv")
+        .option("header", "true")
+        .option("inferSchema", "true")
+        .load(s"$resourceDataPath/retail-data/by-day/*.csv")
 
-    val staticDataFrame = spark.read.format("csv")
-      .option("header", true)
-      .option("inferSchema", "true")
-      .load(s"$resourceDataPath/retail-data/by-day/*.csv")
+      staticDataFrame.createOrReplaceTempView("retail_data")
+      val staticSchema = staticDataFrame.schema
 
-    staticDataFrame.createOrReplaceTempView("retail_data")
-    val staticSchema = staticDataFrame.schema
+      println(staticSchema)
 
-    println(staticSchema)
+
+      import org.apache.spark.sql.functions.{window, column, desc, col}
+      staticDataFrame
+        .selectExpr(
+          "CustomerId",
+          "(UnitPrice * Quantity) as total_cost",
+          "InvoiceDate"
+        )
+        .groupBy(
+          col("CustomerId"), window(col("InvoiceDate"), "1 day")
+        )
+        .sum("total_cost")
+        .show(5)
+
+      val streamingDataFrame = spark.readStream
+        .schema(staticSchema)
+        .option("maxFilesPerTrigger", 1)
+        .format("csv")
+        .option("header", "true")
+        .load(s"$resourceDataPath/retail-data/by-day/*.csv")
+
+      streamingDataFrame.isStreaming
+
+      val purchaseByCustomerPerHour = streamingDataFrame
+        .selectExpr(
+          "CustomerId",
+          "(UnitPrice * Quantity) as total_cost",
+          "InvoiceDate"
+        )
+        .groupBy(
+          $"CustomerId", window(col("InvoiceDate"), "1 day")
+        )
+        .sum("total_cost")
+
+      // FIXME: NPE 발생
+      purchaseByCustomerPerHour.writeStream
+        .format("memory") // memory = store in-memory table
+        .queryName("customer_purchases") // the name of the in-memory table
+        .outputMode("complete") // complete = all the counts should be in the table
+        .start()
+
+      spark.sql(
+          """
+            | SELECT *
+            | FROM customer_purchases
+            | ORDER BY `sum(total_cost)` DESC
+            |""".stripMargin)
+        .show(5)
+    }
+
+    def machineLearningAndAnalytics() = {
+      import org.apache.spark.sql.functions.date_format
+
+      val staticDataFrame = spark.read.format("csv")
+        .option("header", "true")
+        .option("inferSchema", "true")
+        .load(s"$resourceDataPath/retail-data/by-day/*.csv")
+
+      val preppedDataFrame = staticDataFrame
+        .na.fill(0)
+        .withColumn("day_of_week", date_format($"InvoiceDate", "EEEE"))
+        .coalesce(5)
+
+      val trainDataFrame = preppedDataFrame
+        .where("InvoiceDate < '2011-07-01'")
+      val testDataFrame = preppedDataFrame
+        .where("InvoiceDate >= '2011-07-01'")
+
+      println(trainDataFrame.count())
+      println(testDataFrame.count())
+
+      import org.apache.spark.ml.feature.StringIndexer
+      val indexer = new StringIndexer()
+        .setInputCol("day_of_week")
+        .setOutputCol("day_of_week_index")
+
+      println(indexer)
+    }
+    machineLearningAndAnalytics()
 
     // Stop the SparkSession
     spark.stop()
